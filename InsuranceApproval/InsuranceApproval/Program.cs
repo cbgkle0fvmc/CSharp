2f31d9313e162ea76971fc1769fefdbb42d84e3e
Josh Cordova <151390365+cbgkle0fvmc@users.noreply.github.com>
2025-09-24 13:22:44 -0500

    Insurance Approval assignment

---
+++ InsuranceApproval/InsuranceApproval/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApproval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize approval as false until proven true
+            bool approval = false;
+            //ask user for age, dui and tickets input 
+            Console.WriteLine("What is your age?");
+            byte age = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you had?");
+            byte tickets = Convert.ToByte(Console.ReadLine());
+            //use if else loop to provide boolean approval
+            if (age >= 15 && dui == false && tickets < 3)
+            {
+                approval = true;
+            } 
+            else
+            {
+                approval = false;
+            }
+            Console.WriteLine(approval);
+        }
+    }
+}
