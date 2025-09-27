87f97ebae886580295aecea233cd05863fab8ae1
Josh Cordova <151390365+cbgkle0fvmc@users.noreply.github.com>
2025-09-27 09:32:49 -0500

    MathAndComparisonOperator, Iterate through an array assignment
    Add comments to math and comparison operator assignement, steps 1-4 iterate through an array assignment

--- Iterate through an array assignment/Iterate through an array assignment/Program.cs
+++ Iterate through an array assignment/Iterate through an array assignment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,17 +16,73 @@ namespace Iterate_through_an_array_assignment
             //ask user for text input
             Console.WriteLine("Please input some text");
             string additionalText = Console.ReadLine();
-            
+
             //iterate through each string in array and add user input to original string
+            //part two, step 1. (create in infinite loop) this is infinite because the condition is always true
+            //part two, stpe 3. fix the infinite loop (comment out the while loop
+            //while (1 == 1)
+            //{
             for (int i = 0; i < stringArray.Length; i++)
             {
                 stringArray[i] = stringArray[i] + additionalText;
+                if (i <= stringArray.Length - 1)
+                {
+                    Console.WriteLine("----- line break -----");
+                }
             }
             //print new string at each index after user input has been added to the string
             foreach (string array in stringArray)
             {
                 Console.WriteLine(array);
             }
+            //}
+
+            //Part 4. Step 1: Create list of unique strings
+            string[] stringArray2 = { "Treadmill", "Light", "Fan", "Dark", "Moon", "Sticky", "Note", "Screwdriver" };
+
+            //Part 4. Step 2: Ask used for input text to search the list, hold value in variable
+            Console.WriteLine("Please enter a character to seatch for text in the list that contains that character");
+            string userInput = Console.ReadLine();
+
+            //Part 4. Step 3: use for loop to iterate through string array and search for any strings that contain the user input
+            List<string> matchingWords = new List<string>(); //start by instantiating empty list of add names that contain user input (matchingWords)
+
+            //Part 4. Step 4
+            //if word in stringArray2 contains the user input, add the word to the list matchingWords
+            foreach (string word in stringArray2)
+            {
+                if (word.Contains(userInput))
+                {
+                    matchingWords.Add(word);
+
+                }
+            }
+            //Part 4. Step 3. Display the index of the list item that contains userInput
+            for (int j = 0;j < matchingWords.Count;j++)
+            {
+                if (matchingWords[j].Contains(userInput))
+                {
+                    Console.WriteLine("Index " + j + " contains user Input" );
+                }
+            }
+            //Part 4. Step 4: Add code to check if the user put in text that isn't on the list and, if they did, tell 
+            //the user their input is not on the list.
+
+            //if no words match user input, list will contain 0 indexes, if count of list == 0, print"No words contain that character"
+            if (matchingWords.Count == 0)
+            {
+                Console.WriteLine("No words contain that character");
+            }
+            //if count of list != 0, words match. in that case, use  for loopeach loop to print each word in matchingWords to the console
+            else
+            {
+                foreach (string word in matchingWords)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+
         }
     }
 }
--- MathAndComparisonOperator/MathAndComparisonOperator/Program.cs
+++ MathAndComparisonOperator/MathAndComparisonOperator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,25 +11,35 @@ namespace MathAndComparisonOperator
     {
         static void Main(string[] args)
         {
+            //The program must start by printing “Anonymous Income Comparison Program” to the screen
             Console.WriteLine("Anonymous Income Comparison Program");
+            //It must then print “Person 1” to the screen and get the following details from user input:
+            //Hourly Rate
+            //Hours worked per week
             Console.WriteLine("Person 1");
             Console.WriteLine("What is your hourly rate?");
             int person1Hourly = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many hours did you work?");
             int person1Hours = Convert.ToInt32(Console.ReadLine());
 
+            //It must then print “Person 2” to the screen and then get the following details from user input:
+            //Hourly rate
+            //Hours worked per week
             Console.WriteLine("Person 2");
             Console.WriteLine("What is your hourly rate?");
             int person2Hourly = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many hours did you work?");
             int person2Hours = Convert.ToInt32(Console.ReadLine());
 
+            //It must then print to the screen “Annual salary of Person 1:” and display the exact salary below it.
             int person1AnnualSalary = person1Hourly * person1Hours * 52;
+            //It must then print to the screen “Annual salary of Person 2:” and display the exact salary below it.
             int person2AnnualSalary = person2Hourly * person2Hours * 52;
 
             Console.WriteLine("Annual salary person 1: \n" + person1AnnualSalary );
             Console.WriteLine("Annual salary person 2: \n" + person2AnnualSalary);
 
+            //It must then print to the screen “Does Person 1 make more money than Person 2?” and write the true or false value of this statement below it.
             bool person1GreaterThanPerson2 = person1Hourly > person2Hourly;
             Console.WriteLine("Does person 1 make more than person 2? \n" + person1GreaterThanPerson2);
 
