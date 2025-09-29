76a07c65ccbde2705441973e483d75e97eb48f96
Josh Cordova <151390365+cbgkle0fvmc@users.noreply.github.com>
2025-09-29 13:55:11 -0500

    Method Submission Assignment

---
+++ Method_SubmissionAssignment/Method_SubmissionAssignment/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_SubmissionAssignment
+{
+    internal class Math
+    {
+        // STEP 1.
+        // Create a class. In that class, create a method that takes two integers as parameters.
+        // Make one of them optional. Have the method do a math operation and return an integer result.
+        public int OptionalMethod(int num1, int num2=1)
+        {
+            return num1 * num1;
+        }
+    }
+}
---
+++ Method_SubmissionAssignment/Method_SubmissionAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_SubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // STEP 2.
+            // In the Main() method of the console app, instantiate the class.
+            Math math = new Math();
+
+            // STEP 3.
+            // Ask the user to input two numbers, one at a time. Let them know they need not enter
+            // anything for the second number.
+            Console.WriteLine("Please input two numbers, one at a time. You do not need to enter anything for the second number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // STEP 4.
+            // Call the method in the class, passing in the one or two numbers entered.
+            int userInput = math.OptionalMethod(num1, num2);
+            Console.WriteLine(userInput);
+            
+            //STEP 5.
+            //Try various combinations of numbers on the code, including having no second number.
+            Console.WriteLine("Please input two numbers, one at a time. You do not need to enter anything for the second number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int num4 = Convert.ToInt32(Console.ReadLine());
+            int userInput2 = math.OptionalMethod(num3, num4);
+            Console.WriteLine(userInput2);
+
+        }
+    }
+}
