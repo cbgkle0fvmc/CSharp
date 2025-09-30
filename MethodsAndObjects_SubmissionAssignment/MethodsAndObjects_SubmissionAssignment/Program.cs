188942bd5ac0cb0b36b38df8ca34536779a32ff7
Josh Cordova <151390365+cbgkle0fvmc@users.noreply.github.com>
2025-09-30 10:36:46 -0500

    Methods and Objects Submission Assignment

---
+++ MethodsAndObjects_SubmissionAssignment/MethodsAndObjects_SubmissionAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects_SubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // STEP 4. 
+            // Inside of the Main method, instantiate and initialize an Employee object with a
+            // first name of “Sample” and a last name of “Student”.
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student"};
+
+            // STEP 5. 
+            // Call the superclass method SayName() on the Employee object.
+            employee.SayName();
+        }
+    }
+}
